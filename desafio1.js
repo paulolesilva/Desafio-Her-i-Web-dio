b26df228bddd128f2a1469a08f4b3030d53da093
Paulo <paulo2507@hotmail.com>
2023-11-20 10:47:59 -0300

    Desafio web-dio Nível Herío

---
+++ desafio1.js
@@ -0,0 +1,28 @@
+let nome= ['Gladiador', 'Hellraiser', 'Shaman', 'Zealot', 'Samurai', 'Jewel','Frozen','Titanium' ]
+
+let xp = ['Ferro','Bronze', 'Prata', 'Ouro', 'Platina', 'Ascendente','Imortal', 'Radiante']
+
+console.log('o heroi de nome '+ nome[7] + ' está no nível '+xp[7])//código especifico
+
+    if (xp ==='Ferro' && xp< 1000){
+    
+}   else if (xp === 'Bronze'  && xp>= 1001 &&  xp <= 2000){
+   
+}   else if (xp ==='Prata'  && xp>= 2001   && xp<=5000) {
+    
+}   else if (xp === 'Ouro'  && xp >= 6001   && xp<=7000){
+    
+}   else if(xp==='Platina' && xp >= 7001 && xp<=8000){
+    
+}   else if(xp=== 'Ascendente' && xp >= 8001  && xp<= 9000){
+    
+}   else if(xp ==='Imortal' && xp >=9001  && xp<=10000){
+}    
+     if(xp=== 'Radiante' && xp>= 10001){
+    
+}
+console.log('O Herói de nome ' + nome  + ' está no nível ' +xp )//codigo completo
+
+
+ 
+    
